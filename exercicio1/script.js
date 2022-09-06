6314cefbdba529f1fb3d9cc22f31b38c0fdb30ac
Mascariep <edipohc@gmail.com>
2022-09-06 16:55:09 -0300

    exercicios-condicionais-a012

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,24 @@
+/*# Exercício 1
+
+Crie um código que receba uma const numérica qualquer. 
+Crie um `if` para verificar se o número guardado na const é **par**. 
+Caso seja, imprima no console a mensagem: "o número é par".
+
+
 const num = //seu numero aqui
 
 if(){
     console.log("sua mensagem aqui");
 }
+
+*/
+const num = prompt("Qual seu numero?")
+console.log("Seu numero é:", num)
+
+
+if((num % 2)=== 0) {
+    console.log("O número", num, "é par")
+} else {
+    console.log("O número", num, "é ímpar")
+}
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,31 @@
+/*
+# Exercício 3
+
+Crie um código que receba um **prompt** que diga: "Escreva aqui sua nacionalidade" 
+e guarde o valor da resposta em uma const `nacionalidade`.
+
+Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
+- brasileira;
+- argentina;
+- uruguaia;
+- chilena;
+- colombiana;
+
+Crie uma estrutura de `if/else if/else` que verifique se a nacionalidade guardada na const `nacionalidade` é 
+igual a cada uma das nacionalidades acima, e caso seja, imprima a nacionalidade no console. 
+O último `else` deve imprimir "nacionalidade não encontrada" caso o valor de `nacionalidade` não corresponda a nenhum dos valores.
+
+> 💡 Lembre-se de prestar atenção às letras maiúsculas e minúsculas. 
+
+
 let nacionalidade //= adicione o prompt aqui
+*/
+
+let nacionalidade = prompt("Escreva aqui sua nacionalidade?").toLocaleLowerCase()
+console.log("Sua nacionalidade é:", nacionalidade)
+
+if (nacionalidade === "brasileira" || nacionalidade === "argentina" || nacionalidade === "uruguaia" || nacionalidade === "chilena" || nacionalidade === "colombiana") {
+    console.log("Nacionalidade encontrada")
+} else {
+    console.log("Nacionalidade não encontrada")
+}
